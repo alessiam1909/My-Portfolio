4300625e0a2269d2dc3a377fc7a6438ee3197777
alessiam1909 <ale.menon.vr@gmail.com>
2023-04-17 21:19:58 +0200

    sistemazione funzione js

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 new WOW().init();
 
-let menu = document.querySelector('div');
+let menu = document.querySelector('.hamburger');
+let header = document.querySelector('header');
+let openMenu = false;
 menu.addEventListener('click', function (e) {
+    menu.disabled = true;
     menu.classList.toggle('toggleMenu');
+    if (!openMenu) {
+        openMenu = true;
+        header.style.height = '1280px';
+    }
+    else {
+        openMenu = false;
+        header.style.height = '1080px';
+    }
+    menu.disabled = false;
 })
